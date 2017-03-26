af9ace7ec26e6f07f6c51b4bae1347840c53e59c
siddharthmishrase26 <siddharthmishrase26@gmail.com>
2017-03-26 12:39:49 +0530

    Program to find Area of Circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,51 @@
+/**
+*	Method Overloading
+*	
+*/
+import java.util.Scanner;
+
+class Circle {
+
+	private double radius;
+	private double pi;
+	private double area;
+
+		void assign(){
+
+			pi=3.14;
+		}
+
+		void assign(double radius){
+		
+			this.radius=radius;
+		}
+
+		private void calculate(){
+	
+			area=pi*radius*radius;
+		}
+		
+		void output(){
+
+			calculate();
+			System.out.print("Area Of Circle:-"+area);
+		}
+}
+
+class AreaOfCircle{
+
+	public static void main(String [] args){
+	
+		Circle c = new Circle();
+			double r;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter The radius Of Circle:-");
+		r=sc.nextDouble();
+		c.assign();
+		c.assign(r);
+		c.output();
+	}
+}
+
+		
+
